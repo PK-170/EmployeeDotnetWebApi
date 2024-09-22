b085fff40ea5d892e81362eddfea005f44320ed6
PK-170 <pawan59c@gmail.com>
2024-09-22 12:18:31 +1000

    added data for db context

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219fefc2b2b6de861e80d5b86832979991049b91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f19c7ec1709c08bce3539170af8903736d6fcbd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
