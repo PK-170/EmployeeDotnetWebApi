94b30a35eaa84edc8dbbf59b497791ba65cfa00a
PK-170 <pawan59c@gmail.com>
2024-09-24 19:58:27 +1000

    adding Mappers

--- api/Mappers/ContactMappers.cs
+++ api/Mappers/ContactMappers.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dto;
+using api.Models;
 
 namespace api.Mappers
 {
-    public class ContactMappers
+    public static class ContactMappers
     {
-        
+
+        public static ContactDto ToContactDto(this Contact contactModel)
+        {
+
+        }
+
     }
 }
