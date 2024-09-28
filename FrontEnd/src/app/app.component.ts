fc420510b18dfc31f3645f461316405aad1f79ee
PK-170 <pawan59c@gmail.com>
2024-09-28 16:57:50 +1000

    seeting up http calls

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontUI';
+
+  http = inject(HttpClient);
+
+
+
+
+
+  private getContacts()
+  {
+
+  }
 }
