6ebb1eb1450e0194f4a9d13b9c622e9a62356fb9
PK-170 <pawan59c@gmail.com>
2024-09-29 11:29:48 +1000

    added get method in the client side

--- test.ts
+++ test.ts
@@ -1,3 +1,8 @@
 console.log('====================================');
 console.log("TSC");
 console.log('====================================');
+
+var x:number = 100;
+var y:any = 22;
+
+console.log(x)
