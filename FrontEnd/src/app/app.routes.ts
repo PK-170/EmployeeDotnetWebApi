f9b161fa788f85b86367ac926ecb8a6b6e9f7f71
PK-170 <pawan59c@gmail.com>
2024-10-05 19:17:43 +1000

    added component

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '', redirectTo:'Login' , pathMatch:'full'
+},
+{
+    path:'login',
+    component:LoginComponent
+}
+
+
+];
