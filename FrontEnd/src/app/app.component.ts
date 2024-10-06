c9693e18f90957905193abfaf186230a4560fb33
PK-170 <pawan59c@gmail.com>
2024-10-06 11:32:18 +1100

    adding routing

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Contact } from '../models/contact.model';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, AsyncPipe, FormsModule, ReactiveFormsModule, CommonModule, RouterModule, HomeComponent],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe, FormsModule, ReactiveFormsModule, CommonModule, RouterModule, HomeComponent, LoginComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-  
+   title = 'routing-standalone-components';
 
 }
--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 {
-    path: '', component: LoginComponent, title: 'Login Page'
+    path: 'login-component', component: LoginComponent
+},
+{
+    path:'home-component', component:HomeComponent
 }
 
 
