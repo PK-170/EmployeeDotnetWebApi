b64917d22f5f7219ee75730ee9464c29b6731a4f
PK-170 <pawan59c@gmail.com>
2024-10-07 20:30:01 +1100

    adding register

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
     {
@@ -9,6 +10,9 @@ path:'', redirectTo:'login', pathMatch:'full'
     },
 {
     path: 'login', component: LoginComponent
+},
+{
+    path:'register-component', component:RegisterComponent
 },
 {
     path:'home-component', component:HomeComponent
