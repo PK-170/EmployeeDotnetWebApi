58917c1c4874eb981bafd6e9dec5d10b9e1f8faa
PK-170 <pawan59c@gmail.com>
2024-10-07 11:06:58 +1100

    login page working

--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { ToastrService, ToastNoAnimation } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -13,31 +14,58 @@ import { HomeComponent } from '../home/home.component';
 })
 export class LoginComponent {
 
-LoginObj: Login;
-constructor(private router: Router)
+loginObj: Login;
+loginResp: LoginResp;
+
+
+constructor(private router: Router,  private http: HttpClient)
 {
-  this.LoginObj = new Login();
+  this.loginObj = new Login();
+  this.loginResp = new LoginResp();
+  
 }
 
 
 onLogin(){
-  console.log(this.LoginObj.EmilId)
-  console.log(this.LoginObj.Password);
-   this.router.navigateByUrl('/home-component');
   
+  this.http.post('http://localhost:5236/api/account/login', this.loginObj)
+    .subscribe((res)=>{
+            this.loginResp == res;
+            this.router.navigateByUrl('/home-component');    
+      
+  },Error=>{
+    //this.toastr.error("Invalid Username and/or Password");
+    alert("Invalid Username and/or Password");
+    console.log("Invalid Username");
+  }
+);  
 }
   
 }
 
 export class Login{
 
-  EmilId: string;
+  UserName: string;
   Password: string;
 
   constructor()
   {
-    this.EmilId = '';
+    this.UserName = '';
     this.Password ='';
   }
+
+}
+
+export class LoginResp{
+  userName: string;
+  email: string;
+  token: string;
+  constructor()
+  {
+    this.userName = '';
+    this.email = '';
+    this.token = '';
+  }
+
 
 }
